f9a955589a6801b19b1665152879596f97b1d27d
Nathan <nathanbc55@gmail.com>
2023-02-04 16:05:17 -0600

    object classes

---
+++ scripts/Root.js
@@ -0,0 +1,11 @@
+import GameObject from "./GameObject";
+
+export default class Root extends GameObject {
+    constructor() {
+
+    }
+
+    tick() {
+        
+    }
+}
---
+++ scripts/Water.js
@@ -0,0 +1,8 @@
+import GameObject from "./GameObject";
+
+export default class Water extends GameObject {
+    tick() {
+
+    }
+    
+}
