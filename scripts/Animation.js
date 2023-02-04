bdd466714db19b01248a16b21cdce018160feec7
Nathan <nathanbc55@gmail.com>
2023-02-04 16:11:52 -0600

    updated objects

--- scripts/Animation.js
+++ scripts/Animation.js
@@ -1,7 +1,11 @@
 export default class Animation {
-    constructor() {
-
+    constructor(img) {
+        this.img = new Image();
+        this.img.src = img;
     }
 
+    render(ctx) {
+        ctx.drawImage(this.img, this.x, this.y)
+    }
     
 }
