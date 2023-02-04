9d0d3ae838c4e39af767ac83d1d43a386a29405e
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2023-02-03 20:29:03 -0600

    controller stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
-alert("hello");
-
+let gamePadsIndex;
 window.addEventListener("gamepadconnected", (e) => {
-    const gp = navigator.getGamepads()[e.gamepad.index];
+    gamePadIndex = e.gamepad.index;
+    gp = navigator.getGamepads()[e.gamepad.index];
     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
     gp.index, gp.id,
     gp.buttons.length, gp.axes.length);
+    console.log(gp.buttons);
   });
+
+
+function getInfo() {
+    for(let i = 0; i < navigator.getGamepads().length; i++) {
+        console.log(navigator.getGamepads()[i]);
+    }
+}
