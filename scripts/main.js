bdbf0ab1491a46d3f0f082ea6f727c981f18a43a
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2023-02-03 19:39:34 -0600

    log different info

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ alert("hello");
 
 window.addEventListener("gamepadconnected", (e) => {
     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-      e.gamepad.index, e.gamepad.id,
-      e.gamepad.buttons.length, e.gamepad.axes.length);
+    gp.index, gp.id,
+    gp.buttons.length, gp.axes.length);
   });
