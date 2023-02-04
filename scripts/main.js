72c5100cad4795a98e747d6b5c534dc77b8c8e24
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2023-02-03 19:34:35 -0600

    gamepod code

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 alert("hello");
+
+window.addEventListener("gamepadconnected", (e) => {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index, e.gamepad.id,
+      e.gamepad.buttons.length, e.gamepad.axes.length);
+  });
