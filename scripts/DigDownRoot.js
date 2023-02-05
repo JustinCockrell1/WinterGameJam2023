2c741af09b0dd13d85ade725ffd8ff2a2e12c8c9
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2023-02-04 19:26:54 -0600

    refactor

---
+++ scripts/DigDownRoot.js
@@ -0,0 +1,90 @@
+import GameObject from "./GameObject.js";
+import Root from "./Root.js";
+
+export default class DigDownRoot {
+
+
+
+    constructor() {
+        this.gameRunning = false;
+
+        this.gameObjects = [];
+
+        this.gamePadIndex = -1;
+
+
+        this.lastTotalElapsedTime = 0;
+
+        this.ctx;
+
+this.setupEvents();
+    }
+
+    setupEvents() {
+        window.addEventListener("gamepadconnected", (e) => {
+            console.log("controller connected");
+            this.gamePadIndex = e.gamepad.index;
+            // gp = navigator.getGamepads()[e.gamepad.index];
+            // console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+            // gp.index, gp.id,
+            // gp.buttons.length, gp.axes.length);
+            // console.log(gp.buttons);
+          });
+
+          window.addEventListener("gamepaddisconnected", (e) => {
+            console.log("Gamepad disconnected from index %d: %s",
+              e.gamepad.index, e.gamepad.id);
+              this.gamePadIndex = -1;
+          });
+        
+    }
+
+    addGameObject(object) {
+        this.gameObjects.push(object);
+    }
+
+    gameLoop(totalElapsedTime) {
+        //get the time past since the last animation frame and save it in elapsed time
+        const elapsedTime = (totalElapsedTime - this.lastTotalElapsedTime)/1000;
+        this.lastTotalElapsedTime = totalElapsedTime;
+        this.tick(elapsedTime);
+        this.render(this.ctx);
+    
+        window.requestAnimationFrame((totalElapsedTime)=>this.gameLoop(totalElapsedTime));
+      }
+
+      startGame() {
+        this.gameRunning=true;
+        this.gameLoop();
+
+        this.addGameObject(new Root(50,50));
+      }
+    
+
+    tick(elapsedTime) {
+console.log(this.gamePadIndex);
+        if(this.gamePadIndex!=-1) {
+            const gamePad = navigator.getGamepads()[this.gamePadIndex];
+            // console.log(gamePad.axes[0], gamePad.axes[1]);
+            if(gamePad.buttons[0].pressed) this.gameRunning=true;
+    
+            if(this.gameRunning) {
+                for(let i = 0; i < this.gameObjects.length; i++) {
+                    this.gameObjects[i].tick(elapsedTime, gamePad);
+                }
+      
+    
+            }
+       
+     
+        }
+
+    
+    }
+    render() {
+      for(let i = 0; i < this.gameObjects.length; i++) {
+            this.gameObjects[i].render(this.ctx);
+        }
+    }
+
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,84 +1,26 @@
-import Root from "./Root.js";
-
-let gamePadIndex =-1;
-let ctx;
-let root = new Root(200,200);
-let gameRunning = false;
-
-window.addEventListener("gamepadconnected", (e) => {
-    gamePadIndex = e.gamepad.index;
-    // gp = navigator.getGamepads()[e.gamepad.index];
-    // console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    // gp.index, gp.id,
-    // gp.buttons.length, gp.axes.length);
-    // console.log(gp.buttons);
-  });
-
-
-  window.addEventListener("gamepaddisconnected", (e) => {
-    console.log("Gamepad disconnected from index %d: %s",
-      e.gamepad.index, e.gamepad.id);
-      gamePadIndex = -1;
-  });
+import DigDownRoot from "./DigDownRoot.js";
 
 
 
-let lastTotalElapsedTime = 0;
-//Main Game Loop
-  function gameLoop(totalElapsedTime) {
-    //get the time past since the last animation frame and save it in elapsed time
-    const elapsedTime = (totalElapsedTime - lastTotalElapsedTime)/1000;
-    lastTotalElapsedTime = totalElapsedTime;
-    tick(elapsedTime);
-    render();
-
-    window.requestAnimationFrame(gameLoop);
-  }
 
 
-  function tick(elapsedTime) {
-    //Pull in the current state of the controller
-    
-    if(gamePadIndex!=-1) {
-        const gamePad = navigator.getGamepads()[gamePadIndex];
-        // console.log(gamePad.axes[0], gamePad.axes[1]);
-        if(gamePad.buttons[0].pressed) gameRunning=true;
-
-        if(gameRunning)
-        root.tick(elapsedTime, gamePad);
-        //Get the angle of the controller
-  
 
 
-   
- 
-    }
 
-    }
 
-  function render() {
-    root.render(ctx);
-  
-  }
+let game = new DigDownRoot();
+
 
 
   
 
   window.onload = function() {
     const canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+    let ctx = canvas.getContext("2d");
     ctx.canvas.width = 600;
     ctx.canvas.height = 600;
     ctx.fillStyle = "grey";
     ctx.fillRect(0,0, ctx.canvas.width, ctx.canvas.height); 
-    window.requestAnimationFrame(gameLoop);  
+     game.ctx = ctx;
+     game.startGame();
   }
-
-  window.addEventListener("keydown", (e)=>{
-    if(e.key=="LEFT") {
-
-    }
-    else if(e.key=="RIGHT") {
-
-    }
-  });
